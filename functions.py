82f78a6d74ab4dcb903a7edbc7cfc79485d07faa
ahmadx17 <kathafy4hmad@gmail.com>
2026-06-15 12:07:19 +0530

    functions.py is modiefied

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+# exercise 01
 def add():
     print(5+5)
 def sub():
@@ -7,5 +8,30 @@ def mul():
 def dev():
     print(5/5)
 
-sub()    
+# exercise 02
 
+def findevenorode():
+    n=int(input("Enter a number:"))
+    if n%2==0:
+        print (n,"is even number")   
+    else:
+        print(n,"is odd number") 
+
+# exercise 03
+
+def passorfail(n):
+    if n>=35:
+        print("pass")
+    else:
+        print("fail")
+    n=int(input("Enter your score:"))
+
+# exercise 04
+
+def range_for(a,b ):
+    for i in range (a,b ,-1):
+        print (i)
+
+a=int(input("num:"))    
+b=int(input("num:"))
+range_for(a,b)    
